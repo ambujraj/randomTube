143b1ac7f320d3ce3a02eae0a85950ba0dfb60fb
Ambuj <thecoderguy@outlook.com>
2018-07-19 14:59:00 +0530

    minor

--- app.js
+++ app.js
@@ -11,7 +11,7 @@ app.get("/result", function(req, res){
     var query = req.query.user;
     var ab = query.split(" ");
     query = ab.join("+");
-    var url = 'https://www.googleapis.com/youtube/v3/search?part=snippet&maxResults=50&order=date&type=video&q=' + query +'&key=AIzaSyA58s6f45ex23jhsW0m54CpjhD71a4bJgc';
+    var url = 'https://www.googleapis.com/youtube/v3/search?part=snippet&maxResults=50&safeSearch=strict&order=relevance&type=video&q=' + query +'&key=AIzaSyA58s6f45ex23jhsW0m54CpjhD71a4bJgc';
     request(url, function(error, response, body){
          if(!error && response.statusCode == 200){
              var parsed = JSON.parse(body);
