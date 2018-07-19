d323f7370ca9df0fb3ac134e4e1f1eace462c6a5
Ambuj <thecoderguy@outlook.com>
2018-07-19 15:27:20 +0530

    minor

--- app.js
+++ app.js
@@ -11,13 +11,24 @@ app.get("/result", function(req, res){
     var query = req.query.user;
     var ab = query.split(" ");
     query = ab.join("+");
+    if(query=="ambuj+raj"|| query=="Ambuj+Raj"|| query=="ambuj" || query=="Ambuj" || query=="ambujraj"){
+        var url = 'https://www.googleapis.com/youtube/v3/search?part=snippet&channelId=UCNj8krJYML-KG22xyumvMww&order=date&type=video&key=AIzaSyA58s6f45ex23jhsW0m54CpjhD71a4bJgc';
+        request(url, function (error, response, body) {
+            if (!error && response.statusCode == 200) {
+                var parsed = JSON.parse(body);
+                res.render("search", { search: parsed, amb: "Awesome !!" });
+            }
+        });
+    }
+    else{
     var url = 'https://www.googleapis.com/youtube/v3/search?part=snippet&maxResults=50&safeSearch=strict&order=relevance&type=video&q=' + query +'&key=AIzaSyA58s6f45ex23jhsW0m54CpjhD71a4bJgc';
     request(url, function(error, response, body){
          if(!error && response.statusCode == 200){
              var parsed = JSON.parse(body);
-             res.render("search", {search: parsed});
+             res.render("search", {search: parsed, amb: ""});
          }
     });
+}
 });
 app.get("*", function(req, res){
         res.redirect("/");
