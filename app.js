f2b4bf03d1534d4f7ec59483ca0b27f2197a353c
Ambuj <thecoderguy@outlook.com>
2018-07-20 13:55:22 +0530

    mobile-optimise

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ const request = require('request');
 const bodyParser = require('body-parser');
 app.use(bodyParser.urlencoded({extended: true}));
 app.set("view engine", "ejs");
+app.use(express.static("public"));
 app.get("/", function(req, res){
    res.render("home");
 });
