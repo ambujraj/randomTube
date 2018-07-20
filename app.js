c6020a5ce6668949d263bca9868258cf693cdd85
Ambuj <thecoderguy@outlook.com>
2018-07-21 00:16:02 +0530

    bug fixes

--- app.js
+++ app.js
@@ -9,11 +9,11 @@ app.get("/", function(req, res){
    res.render("home");
 });
 app.get("/result", function(req, res){
-    var query = req.query.user;
-    console.log(query);
-    var ab = query.split(" ");
-    query = ab.join("+");
-    if(query=="ambuj+raj"|| query=="Ambuj+Raj"|| query=="ambuj" || query=="Ambuj" || query=="ambujraj"){
+    var queryy = req.query.query;
+    console.log(queryy);
+    var ab = queryy.split(" ");
+    queryy = ab.join("+");
+    if(queryy=="ambuj+raj"|| queryy=="Ambuj+Raj"|| queryy=="ambuj" || queryy=="Ambuj" || queryy=="ambujraj"){
         var url = 'https://www.googleapis.com/youtube/v3/search?part=snippet&channelId=UCNj8krJYML-KG22xyumvMww&order=date&type=video&key=AIzaSyA58s6f45ex23jhsW0m54CpjhD71a4bJgc';
         request(url, function (error, response, body) {
             if (!error && response.statusCode == 200) {
@@ -23,7 +23,7 @@ app.get("/result", function(req, res){
         });
     }
     else{
-    var url = 'https://www.googleapis.com/youtube/v3/search?part=snippet&maxResults=50&safeSearch=strict&order=relevance&type=video&q=' + query +'&key=AIzaSyA58s6f45ex23jhsW0m54CpjhD71a4bJgc';
+    var url = 'https://www.googleapis.com/youtube/v3/search?part=snippet&maxResults=50&safeSearch=strict&order=relevance&type=video&q=' + queryy +'&key=AIzaSyA58s6f45ex23jhsW0m54CpjhD71a4bJgc';
     request(url, function(error, response, body){
          if(!error && response.statusCode == 200){
              var parsed = JSON.parse(body);
