21fd64ce34aae9ec2806c5db841a04f0195cd8c5
Ambuj <thecoderguy@outlook.com>
2018-08-01 19:22:38 +0530

    Update app.js

--- app.js
+++ app.js
@@ -18,7 +18,7 @@ app.get("/result", function(req, res){
     var ab = queryy.split(" ");
     queryy = ab.join("+");
     if(queryy=="ambuj+raj"|| queryy=="Ambuj+Raj"|| queryy=="ambuj" || queryy=="Ambuj" || queryy=="ambujraj"){
-        var url = 'https://www.googleapis.com/youtube/v3/search?part=snippet&channelId=UCNj8krJYML-KG22xyumvMww&order=date&type=video&key=AIzaSyA58s6f45ex23jhsW0m54CpjhD71a4bJgc';
+        var url = 'https://www.googleapis.com/youtube/v3/search?modestbranding=1&part=snippet&channelId=UCNj8krJYML-KG22xyumvMww&order=date&type=video&key=AIzaSyA58s6f45ex23jhsW0m54CpjhD71a4bJgc';
         request(url, function (error, response, body) {
             if (!error && response.statusCode == 200) {
                 var parsed = JSON.parse(body);
