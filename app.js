5835295bdbd957bfce453fe181f1ec23a773d2d3
Ambuj <thecoderguy@outlook.com>
2018-08-17 19:58:11 +0530

    major

--- app.js
+++ app.js
@@ -1,4 +1,3 @@
-
 const express = require('express');
 const app = express();
 const request = require('request');
@@ -12,6 +11,7 @@ var querySchema = new mongoose.Schema({
 });
 app.enable('trust proxy');
 var hit = 1;
+var parsedpix ='';
 var Query = mongoose.model("Query", querySchema);
 app.use(bodyParser.urlencoded({extended: true}));
 app.set("view engine", "ejs");
@@ -20,32 +20,30 @@ app.get("/", function(req, res){
    res.render("home");
 });
 app.get("/result", function(req, res){
+    
     var queryy = req.query.query;
 	
 	var ip= req.ip;
  Query.create({query: queryy, hits: hit, ip: ip});
     var ab = queryy.split(" ");
     queryy = ab.join("+");
-    if(queryy=="ambuj+raj"|| queryy=="Ambuj+Raj"|| queryy=="ambuj" || queryy=="Ambuj" || queryy=="ambujraj"){
-        var url = 'https://www.googleapis.com/youtube/v3/search?part=snippet&channelId=UCNj8krJYML-KG22xyumvMww&order=date&type=video&key=AIzaSyA58s6f45ex23jhsW0m54CpjhD71a4bJgc';
-        request(url, function (error, response, body) {
-            if (!error && response.statusCode == 200) {
-                var parsed = JSON.parse(body);
-		hit++;
-                res.render("search", { search: parsed});
-            }
-        });
-    }
-    else{
-    var url = 'https://www.googleapis.com/youtube/v3/search?part=snippet&maxResults=50&safeSearch=strict&order=relevance&type=video&q=' + queryy +'&key=AIzaSyA58s6f45ex23jhsW0m54CpjhD71a4bJgc';
+    var urlpix = 'https://pixabay.com/api/videos/?key=9494342-0f51f6ed3422203dd259dafaa&q='+ queryy +'&pretty=true';
+    var url = 'https://www.googleapis.com/youtube/v3/search?part=snippet&maxResults=25&safeSearch=strict&order=relevance&type=video&q=' + queryy + '&key=AIzaSyA58s6f45ex23jhsW0m54CpjhD71a4bJgc';
+    //var urlpix = 'https://pixabay.com/api/videos/?key=9494342-0f51f6ed3422203dd259dafaa&q='+queryy+'&per_page=25&order=latest';
+    request(urlpix, function(erro, respo, bod){
+        if(!erro && respo.statusCode == 200){
+           parsedpix = JSON.parse(bod);
+            
+        }
+    });
     request(url, function(error, response, body){
          if(!error && response.statusCode == 200){
              var parsed = JSON.parse(body);
 		hit++;
-             res.render("search", {search: parsed});
+             res.render("search", {search: parsed, pixx: parsedpix});
          }
     });
-}
+
 });
 app.get("/trend", function(req, res){
     var u = 'https://www.googleapis.com/youtube/v3/videos?part=snippet&chart=mostPopular&regionCode=IN&maxResults=26&key=AIzaSyA58s6f45ex23jhsW0m54CpjhD71a4bJgc';
